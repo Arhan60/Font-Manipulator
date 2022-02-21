e12b737f6666a1726ee2132c7a4b85aa3ac887f6
Arhan60 <85107720+Arhan60@users.noreply.github.com>
2022-02-21 14:54:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -11,10 +16,21 @@ function modelLoaded(){
 }
 function draw(){
     background('#ADD8E6');
+    textSize(difference);
+    fill('red');
+    text("Arhan",noseX,noseY);
+
 
 }
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = "+noseX+"noseY = "+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        righttWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX= "+leftWristX+", rightWristX= "+rightWristX+", difference= "+difference);
     }
 }
